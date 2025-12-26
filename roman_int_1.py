8779dcbebc2bfa2c637a062ffcb077082256789e
Anshori K <anshorikasri.4@gmail.com>
2025-12-26 23:03:44 +0700

    penambahan file baru roman -> integer

---
+++ roman_int_1.py
@@ -0,0 +1,28 @@
+# 26.12.2025
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dict_s={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, "M":1000}
+        arr_s=list(s)
+        arr_balik=arr_s[::-1]
+        # print(arr_balik)/// s='VIII' -> arr_balik=['I', 'I', 'I', 'V']
+        print(list(reversed(s)))
+        val= dict_s[arr_balik[0]]
+        # print(val)
+        n=len(arr_balik)
+        pointer=0
+        for i in range(n-1):
+            if dict_s[arr_balik[i]] == dict_s[arr_balik[i+1]]:
+                val = val + dict_s[arr_balik[i+1]]
+                print(dict_s[arr_balik[i]])
+                print(dict_s[arr_balik[i+1]])
+            if dict_s[arr_balik[i]] < dict_s[arr_balik[i+1]]:
+                val = val + dict_s[arr_balik[i+1]]
+            if dict_s[arr_balik[i]] > dict_s[arr_balik[i+1]]:
+                val = val - dict_s[arr_balik[i+1]]
+            # if i == n-1:
+            #     break
+        return val
+
+        
+
+        
