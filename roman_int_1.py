2b9dd40ffa67d012d3532ce0eb30a0b82e4511e7
Anshori K <anshorikasri.4@gmail.com>
2025-12-27 20:47:30 +0700

    merapikan format, dan menghapus beberapa baris yang menjadi komentar

--- roman_int_1.py
+++ roman_int_1.py
@@ -2,14 +2,14 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         dict_s={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, "M":1000}
+        
         arr_s=list(s)
         arr_balik=arr_s[::-1]
         # print(arr_balik)/// s='VIII' -> arr_balik=['I', 'I', 'I', 'V']
         print(list(reversed(s)))
-        val= dict_s[arr_balik[0]]
-        # print(val)
+
         n=len(arr_balik)
-        # pointer=0    <-- i think this is not use
+        val= dict_s[arr_balik[0]]
         for i in range(n-1):
             if dict_s[arr_balik[i]] == dict_s[arr_balik[i+1]]:
                 val = val + dict_s[arr_balik[i+1]]
@@ -19,8 +19,6 @@ class Solution:
                 val = val + dict_s[arr_balik[i+1]]
             if dict_s[arr_balik[i]] > dict_s[arr_balik[i+1]]:
                 val = val - dict_s[arr_balik[i+1]]
-            # if i == n-1:
-            #     break
         return val
 
         
