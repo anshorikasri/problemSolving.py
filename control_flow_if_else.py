8ab4116d5242f637b2d54790e8205be328e2062c
Anshori K <anshorikasri.4@gmail.com>
2026-01-02 21:12:13 +0700

    control flow using if-else

---
+++ control_flow_if_else.py
@@ -0,0 +1,17 @@
+#start: 01.01.2026
+# title: control flow
+
+# we can use: various way for using if-else
+# === === === ===
+
+# 1. if-else
+ifels_n = [2, 3, 5, 7, 8]
+ans=[]
+
+for n in ifels_n:
+    if n%2==0:
+        ans.append('even')
+    else:
+        ans.append('odd')
+        
+print(ans)
