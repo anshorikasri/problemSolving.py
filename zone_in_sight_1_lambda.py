da69d4523bc3053d2c55a2c258259af6c2d8b0e6
Anshori K <anshorikasri.4@gmail.com>
2026-01-06 09:20:12 +0700

    class ini masih belum berfungsi

---
+++ zone_in_sight_1_lambda.py
@@ -0,0 +1,22 @@
+#lesson:lambda
+#start: 06.01.2026
+# =========
+
+# IT CAN NOT WORK, BECAUSE WITHOUT 'self'
+
+class Variatif:
+
+    # traditional
+    def addFive(n):
+        return n+5
+    
+
+    #lambda
+    addSix= lambda x: x+6
+
+coba1 = Variatif()
+
+coba1.addFive(2)
+coba1.addSix(2)
+
+
