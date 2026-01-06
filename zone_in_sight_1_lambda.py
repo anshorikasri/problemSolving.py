1714db62efc532fe25b4623d956e24cb9da97ec6
Anshori K <anshorikasri.4@gmail.com>
2026-01-06 09:25:05 +0700

    lambda function

--- zone_in_sight_1_lambda.py
+++ zone_in_sight_1_lambda.py
@@ -5,15 +5,14 @@
 # IT CAN WORK, BECAUSE WITH 'self'
 
 class Variatif:
-
     # traditional
     def addFive(self,n):
         return n+5
-    
 
     #lambda
     addSix= lambda self,x: x+6
 
+# instance
 coba1 = Variatif()
 
 ans5 = coba1.addFive(2)
