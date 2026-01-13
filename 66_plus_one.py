623089c3b684aaa3ac07f0d674d602fb8bdc86b5
Anshori K <anshorikasri.4@gmail.com>
2026-01-13 08:39:13 +0700

    plus one to array and make it array again,, [1,2,3] --> [1,2,4]

---
+++ 66_plus_one.py
@@ -0,0 +1,32 @@
+#start: 12.01.2026
+# title: 66. plus one
+
+# === === === ===
+
+class Solution:
+    def plusOne(self, digits: list[int]):
+        balikArray= digits[::-1]
+
+        # print(balikArray)
+        banyakDigit = len(balikArray)
+        # print(banyakDigit)
+        
+        total = 0
+        for i,n in enumerate(balikArray):
+            total+= n*(10**(i))
+            # print(n)
+            # print(i)
+ 
+        # print(total)
+        total+=1
+
+        # ngetes aja
+        # print(str(total)[1]+'b')
+
+        #ini hasil output adalah object arr
+        arr_object = (map(int , str(total))) 
+        
+        array = list(arr_object)
+        # print(array)
+        return array
+
