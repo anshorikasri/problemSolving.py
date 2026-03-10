831e3ff923d5599abbef76334c402975ae4e99ee
Anshori K <anshorikasri.4@gmail.com>
2026-03-10 09:42:23 +0700

    this version fix the last before

--- find_unique_binary_string_1980_b.py
+++ find_unique_binary_string_1980_b.py
@@ -3,9 +3,11 @@ class Solution:
 
         res = ""
 
-        for i in range(len(nums)):
-            res += '1' if nums[i][i]=='0' else '0'
-        
+        for i in range(len(nums[0])):
+            if i < len(nums):
+                res += '1' if nums[i][i]=='0' else '0'
+            else:
+                res += '0'  #or you can also res+='1'
         
         return res
 
@@ -13,5 +15,5 @@ class Solution:
 # instance
 jwb = Solution()
 
-jwb1 = jwb.findDifferentBinaryString(['1010' , '1001','0001' , '0011'])
+jwb1 = jwb.findDifferentBinaryString(['1010' , '1101','0001'])
 print(jwb1)
